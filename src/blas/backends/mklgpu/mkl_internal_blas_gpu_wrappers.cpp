6803df9d6723b3a1abbf8b60215b9d76ca698ef2
Meterelliyoz, Mesut <mesut.meterelliyoz@intel.com>
2020-05-20 17:30:13 -0700

    fixme: Windows issues

--- src/blas/backends/mklgpu/mkl_internal_blas_gpu_wrappers.cpp
+++ src/blas/backends/mklgpu/mkl_internal_blas_gpu_wrappers.cpp
@@ -2398,21 +2398,20 @@ cl::sycl::event gemm_batch(cl::sycl::queue &queue, transpose *transa, transpose 
         coalesced_events.push_back(gemm_event);
         total_groupsize += groupsize[i];
     }
-}
 #ifdef _WIN64
-for (int64_t j = 0; j < group_count; j++)
-    *coalesced_events[j]->wait();
-return new cl::sycl::event();
-#else
-cl::sycl::event *return_event = new cl::sycl::event(queue.submit([&](cl::sycl::handler &cgh) {
     for (int64_t j = 0; j < group_count; j++)
-        cgh.depends_on(*coalesced_events[j]);
-    cgh.single_task<class dgemm_batch_coalesce_events_kernel>([]() {
-    });
-}));
-return *return_event;
+        *coalesced_events[j]->wait();
+    return new cl::sycl::event();
+#else
+    cl::sycl::event *return_event = new cl::sycl::event(queue.submit([&](cl::sycl::handler &cgh) {
+        for (int64_t j = 0; j < group_count; j++)
+            cgh.depends_on(*coalesced_events[j]);
+        cgh.single_task<class dgemm_batch_coalesce_events_kernel>([]() {
+        });
+    }));
+    return *return_event;
 #endif
-} // namespace internal
+}
 
 cl::sycl::event gemm_batch(cl::sycl::queue &queue, transpose *transa, transpose *transb, int64_t *m,
                            int64_t *n, int64_t *k, std::complex<float> *alpha,
@@ -2432,21 +2431,20 @@ cl::sycl::event gemm_batch(cl::sycl::queue &queue, transpose *transa, transpose 
         coalesced_events.push_back(gemm_event);
         total_groupsize += groupsize[i];
     }
-}
 #ifdef _WIN64
-for (int64_t j = 0; j < group_count; j++)
-    *coalesced_events[j]->wait();
-return new cl::sycl::event();
-#else
-cl::sycl::event *return_event = new cl::sycl::event(queue.submit([&](cl::sycl::handler &cgh) {
     for (int64_t j = 0; j < group_count; j++)
-        cgh.depends_on(*coalesced_events[j]);
-    cgh.single_task<class cgemm_batch_coalesce_events_kernel>([]() {
-    });
-}));
-return *return_event;
+        *coalesced_events[j]->wait();
+    return new cl::sycl::event();
+#else
+    cl::sycl::event *return_event = new cl::sycl::event(queue.submit([&](cl::sycl::handler &cgh) {
+        for (int64_t j = 0; j < group_count; j++)
+            cgh.depends_on(*coalesced_events[j]);
+        cgh.single_task<class cgemm_batch_coalesce_events_kernel>([]() {
+        });
+    }));
+    return *return_event;
 #endif
-} // namespace mklgpu
+}
 
 cl::sycl::event gemm_batch(cl::sycl::queue &queue, transpose *transa, transpose *transb, int64_t *m,
                            int64_t *n, int64_t *k, std::complex<double> *alpha,
@@ -2466,21 +2464,20 @@ cl::sycl::event gemm_batch(cl::sycl::queue &queue, transpose *transa, transpose 
         coalesced_events.push_back(gemm_event);
         total_groupsize += groupsize[i];
     }
-}
 #ifdef _WIN64
-for (int64_t j = 0; j < group_count; j++)
-    *coalesced_events[j]->wait();
-return new cl::sycl::event();
-#else
-cl::sycl::event *return_event = new cl::sycl::event(queue.submit([&](cl::sycl::handler &cgh) {
     for (int64_t j = 0; j < group_count; j++)
-        cgh.depends_on(*coalesced_events[j]);
-    cgh.single_task<class zgemm_batch_coalesce_events_kernel>([]() {
-    });
-}));
-return *return_event;
+        *coalesced_events[j]->wait();
+    return new cl::sycl::event();
+#else
+    cl::sycl::event *return_event = new cl::sycl::event(queue.submit([&](cl::sycl::handler &cgh) {
+        for (int64_t j = 0; j < group_count; j++)
+            cgh.depends_on(*coalesced_events[j]);
+        cgh.single_task<class zgemm_batch_coalesce_events_kernel>([]() {
+        });
+    }));
+    return *return_event;
 #endif
-} // namespace onemkl
+}
 
 cl::sycl::event gemmt(cl::sycl::queue &queue, uplo upper_lower, transpose transa, transpose transb,
                       int64_t n, int64_t k, float alpha, const float *a, int64_t lda,
