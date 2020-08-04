d6b39f817202d90494efcdada972d09efccdcdf8
Malia, Viraj <viraj.malia@intel.com>
2020-08-04 15:50:46 -0700

    More namespace fixes

--- scripts/generate_cmake.py
+++ scripts/generate_cmake.py
@@ -38,7 +38,7 @@ Usage:
 
 Example:
 
-    {script}  include/onemkl/blas/detail/mklgpu mklgpu
+    {script}  include/oneapi/mkl/blas/detail/mklgpu mklgpu
 '''.format(script = argv[0]))
 
 if len(argv) <= 2:
