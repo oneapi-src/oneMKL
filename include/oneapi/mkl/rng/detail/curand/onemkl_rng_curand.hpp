c54190c8466cf2822234755d0f23b873e0773f11
Vincent R. Pascuzzi <vrpascuzzi@lbl.gov>
2020-12-02 10:35:06 -0800

    Add auto-generated header files for curand
    Header files generated by
    `scripts/generate_backend_api.py`

---
+++ include/oneapi/mkl/rng/detail/curand/onemkl_rng_curand.hpp
@@ -0,0 +1,19 @@
+//
+// Generated based on include/oneapi/mkl/rng.hpp
+//
+
+#pragma once
+
+#include <CL/sycl.hpp>
+
+#include <complex>
+#include <cstdint>
+
+#include "oneapi/mkl/types.hpp"
+namespace oneapi {
+namespace mkl {
+namespace rng {
+namespace curand {} // namespace curand
+} // namespace rng
+} // namespace mkl
+} // namespace oneapi
