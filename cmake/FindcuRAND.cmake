4632d185d0e214de7b639b25ca92db34aded4d43
Vincent R. Pascuzzi <vrpascuzzi@lbl.gov>
2021-03-09 07:36:31 -0800

    Fix FindcuRAND.cmake
    Use correct variables as per #72.

--- cmake/FindcuRAND.cmake
+++ cmake/FindcuRAND.cmake
@@ -18,8 +18,6 @@
 #===============================================================================
 
 find_package(CUDA 10.0 REQUIRED)
-enable_language(CUDA)
-find_path(CURAND_INCLUDE_DIR "curand.h" HINTS ${CMAKE_CUDA_TOOLKIT_INCLUDE_DIRECTORIES})
 get_filename_component(SYCL_BINARY_DIR ${CMAKE_CXX_COMPILER} DIRECTORY)
 # the OpenCL include file from cuda is opencl 1.1 and it is not compatible with DPC++
 # the OpenCL include headers 1.2 onward is required. This is used to bypass NVIDIA OpenCL headers
@@ -28,9 +26,6 @@ HINTS
 ${OPENCL_INCLUDE_DIR}
 ${SYCL_BINARY_DIR}/../include/sycl/
 )
-find_library(CURAND_LIBRARY curand)
-find_library(CUDA_DRIVER_LIBRARY cuda)
-
 # this is work around to avoid duplication half creation in both cuda and SYCL
 add_compile_definitions(CUDA_NO_HALF)
 
@@ -39,19 +34,18 @@ find_package(Threads REQUIRED)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(cuRAND
     REQUIRED_VARS
-        CURAND_LIBRARY
-        CURAND_INCLUDE_DIR
-        CUDA_INCLUDE_DIRS
+	CUDA_TOOLKIT_INCLUDE
+	CUDA_curand_LIBRARY
         CUDA_LIBRARIES
-        CUDA_DRIVER_LIBRARY
+        CUDA_CUDA_LIBRARY
         OPENCL_INCLUDE_DIR
 )
 if(NOT TARGET ONEMKL::cuRAND::cuRAND)
   add_library(ONEMKL::cuRAND::cuRAND SHARED IMPORTED)
   set_target_properties(ONEMKL::cuRAND::cuRAND PROPERTIES
-      IMPORTED_LOCATION ${CURAND_LIBRARY}
-      INTERFACE_INCLUDE_DIRECTORIES "${OPENCL_INCLUDE_DIR};${CUDA_INCLUDE_DIRS}"
-      INTERFACE_LINK_LIBRARIES "Threads::Threads;${CUDA_DRIVER_LIBRARY};${CUDA_LIBRARIES}"
+      IMPORTED_LOCATION ${CUDA_curand_LIBRARY}
+      INTERFACE_INCLUDE_DIRECTORIES "${OPENCL_INCLUDE_DIR};${CUDA_TOOLKIT_INCLUDE}"
+      INTERFACE_LINK_LIBRARIES "Threads::Threads;${CUDA_CUDA_LIBRARY};${CUDA_LIBRARIES}"
   )
 
 endif()
