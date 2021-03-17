c0fc0795303547bec3568b5cab0be22ebe5ddf39
Vincent R. Pascuzzi <vrpascuzzi@lbl.gov>
2021-03-17 06:29:19 -0700

    Add LBL license to cuRAND files

--- src/rng/backends/curand/mrg32k3a.cpp
+++ src/rng/backends/curand/mrg32k3a.cpp
@@ -1,21 +1,20 @@
-/*******************************************************************************
-* Copyright 2020-2021 Intel Corporation
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions
-* and limitations under the License.
-*
-*
-* SPDX-License-Identifier: Apache-2.0
-*******************************************************************************/
+/*********************************************************************************
+* Intel Math Kernel Library (oneMKL) Copyright (c) 2021, The Regents of
+* the University of California, through Lawrence Berkeley National
+* Laboratory (subject to receipt of any required approvals from the U.S.
+* Dept. of Energy). All rights reserved.
+* 
+* If you have questions about your rights to use or distribute this software,
+* please contact Berkeley Lab's Intellectual Property Office at
+* IPO@lbl.gov.
+* 
+* NOTICE.  This Software was developed under funding from the U.S. Department
+* of Energy and the U.S. Government consequently retains certain rights.  As
+* such, the U.S. Government has been granted for itself and others acting on
+* its behalf a paid-up, nonexclusive, irrevocable, worldwide license in the
+* Software to reproduce, distribute copies to the public, prepare derivative 
+* works, and perform publicly and display publicly, and to permit others to do so.
+*********************************************************************************/
 
 #include <iostream>
 #include <CL/sycl.hpp>
