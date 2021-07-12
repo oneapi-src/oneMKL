57a9da424155c8f0a0853be2a3191f10621fca93
Meterelliyoz, Mesut <mesut.meterelliyoz@intel.com>
2021-07-12 15:12:57 -0700

    [BLAS] Fix copyright

--- src/blas/function_table.hpp
+++ src/blas/function_table.hpp
@@ -1,5 +1,4 @@
 /*******************************************************************************
-g
 * Copyright 2020-2021 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
