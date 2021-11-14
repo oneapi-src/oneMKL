939086b9a5754aa6eacb4e03b4e4e5b10a237c21
sbalint98 <sbsbalint14@gmail.com>
2021-11-14 12:23:31 +0100

    Use __SYCL_DEVICE_ONLY__ instead of SYCL_DEVICE_ONLY

--- src/blas/backends/mklcpu/mklcpu_common.hpp
+++ src/blas/backends/mklcpu/mklcpu_common.hpp
@@ -47,7 +47,7 @@ static inline auto host_task_internal(H &cgh, F f, int) -> decltype(cgh.run_on_h
 
 template <typename K, typename H, typename F>
 static inline void host_task_internal(H &cgh, F f, long) {
-#ifndef SYCL_DEVICE_ONLY
+#ifndef __SYCL_DEVICE_ONLY__
     cgh.template single_task<K>(f);
 #endif
 }
