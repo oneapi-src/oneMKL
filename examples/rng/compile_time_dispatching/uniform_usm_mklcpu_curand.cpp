ca34cad480bbfabef870624fd5527221505f0a8c
Meng Cao <mengcao@andrew.cmu.edu>
2022-05-11 20:45:37 -0400

    [BLAS,RNG,LAPACK] Add examples with CMake (#188)
    * [blas] add source files for gemm example
    
    * [blas] separate helper functions into .hpp
    
    * [blas] add device description in header comment
    
    * [blas] update source file name to match public backend name
    
    * [blas] update file names
    
    * [blas] change banner to apache, and move source files into RT/CT folders in domain
    
    * [blas] add source files for gemm example
    
    * [blas] separate helper functions into .hpp
    
    * [blas] add device description in header comment
    
    * [blas] update source file name to match public backend name
    
    * [blas] update file names
    
    * [blas] change banner to apache, and move source files into RT/CT folders in domain
    
    * move examples common helper file under examples/
    
    * [header] update helper header path in src
    
    * update cmakelists for CT and levels up
    
    * RT gemm update include header name
    
    * test change1 cmake
    
    * [blas][rng] fixed examples, tested on lnx
    
    * Added lapack examples and other small fixes
    
    * Create draft examples README.md (not for users)
    
    * Update examples/README.md for better format
    
    * remove detecting L0/openCL for intel backends
    
    * add ctest and set SYCL_DEVICE_FILTER for examples testing
    
    * small fixes with ctest
    
    * Update examples README for users
    
    * Update building_the_project.rst
    
    * add_test for each SYCL_DEVICE_FILTER and align example test name
    
    * fix typo
    
    * improve format of output
    
    * Update examples README output format
    
    * Delete common_helper_functions.hpp
    
    * fix cmake message
    
    * Update examples/blas/CMakeLists.txt
    
    Co-authored-by: Andrew T. Barker <andrew1.barker@intel.com>
    
    * Update examples/blas/compile_time_dispatching/CMakeLists.txt
    
    Co-authored-by: Andrew T. Barker <andrew1.barker@intel.com>
    
    * [blas] remove standalone CT for every backend and add mklcpu+cuda, use malloc_device
    
    * [rng] remove standalone CT for every backend and add mklcpu+cuda, use malloc_device
    
    * [blas][include] improve style
    
    * [lapack] remove standalone CT and add mklcpu+cuda, use malloc_device, and clean up
    
    * small fix in output message
    
    * Update README.md
    
    * style fix
    
    * [all] change calloc to std::vector
    
    * [all] wait_and_throw on events and reverse sycl::free order
    
    * [all] clang-format cpp and hpp files
    
    * set build_examples to true as default, correct year in liscence
    
    * remove not needed files
    
    * remove unnecessary headers
    
    * improve headers
    
    * add comments about CT examples need CUDA backends
    
    * Update README.md
    
    * print matrices instead of check for correctness
    
    * add note about float precision limit in README
    
    * update CMake to work on AMD
    
    * remove extra if
    
    * clean up(improve wording, remove -DNOMINMAX)
    
    * clean up with wording
    
    Co-authored-by: Andrew T. Barker <andrew1.barker@intel.com>

---
+++ examples/rng/compile_time_dispatching/uniform_usm_mklcpu_curand.cpp
@@ -0,0 +1,222 @@
+/*******************************************************************************
+* Copyright 2022 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions
+* and limitations under the License.
+*
+*
+* SPDX-License-Identifier: Apache-2.0
+*******************************************************************************/
+
+/*
+*
+*  Content:
+*       This example demonstrates use of DPC++ API oneapi::mkl::rng::uniform distribution
+*       with oneapi::mkl::rng::philox4x32x10 random number generator to produce
+*       random numbers on a INTEL CPU SYCL device and an NVIDIA GPU SYCL device
+*       with Unified Shared Memory(USM) API.
+*
+*       This example demonstrates only single precision (float) data type
+*       for random numbers
+*
+*******************************************************************************/
+
+// stl includes
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+// oneMKL/SYCL includes
+#include <CL/sycl.hpp>
+#include "oneapi/mkl.hpp"
+
+// local includes
+#include "example_helper.hpp"
+
+//
+// Main example for Uniform random number generation consisting of
+// initialization of random number engine philox4x32x10 object, distribution
+// object. Then random number generation performed and
+// the output is post-processed and validated.
+//
+void run_uniform_example(const sycl::device& cpu_dev, const sycl::device& gpu_dev) {
+    //
+    // Initialization
+    //
+    // example parameters defines
+    constexpr std::uint64_t seed = 777;
+    constexpr std::size_t n = 1000;
+    constexpr std::size_t n_print = 10;
+    constexpr std::size_t alignment = 64;
+
+    // Catch asynchronous exceptions for CPU and GPU
+    auto cpu_exception_handler = [](sycl::exception_list exceptions) {
+        for (std::exception_ptr const& e : exceptions) {
+            try {
+                std::rethrow_exception(e);
+            }
+            catch (sycl::exception const& e) {
+                std::cerr << "Caught asynchronous SYCL exception on CPU device during generation:"
+                          << std::endl;
+                std::cerr << "\t" << e.what() << std::endl;
+            }
+        }
+        std::exit(2);
+    };
+    auto gpu_exception_handler = [](sycl::exception_list exceptions) {
+        for (std::exception_ptr const& e : exceptions) {
+            try {
+                std::rethrow_exception(e);
+            }
+            catch (sycl::exception const& e) {
+                std::cerr << "Caught asynchronous SYCL exception on GPU device during generation:"
+                          << std::endl;
+                std::cerr << "\t" << e.what() << std::endl;
+            }
+        }
+        std::exit(2);
+    };
+
+    // set scalar Type values
+    float a(0.0);
+    float b(10.0);
+
+    // preparation on CPU device and GPU device
+    sycl::queue cpu_queue(cpu_dev, cpu_exception_handler);
+    sycl::queue gpu_queue(gpu_dev, gpu_exception_handler);
+    oneapi::mkl::rng::default_engine cpu_engine(
+        oneapi::mkl::backend_selector<oneapi::mkl::backend::mklcpu>{ cpu_queue }, seed);
+    oneapi::mkl::rng::default_engine gpu_engine(
+        oneapi::mkl::backend_selector<oneapi::mkl::backend::curand>{ gpu_queue }, seed);
+
+    oneapi::mkl::rng::uniform<float> distribution(a, b);
+
+    //
+    // Data preparation on host: prepare array for random numbers
+    //
+    std::vector<float> r_cpu(n);
+    std::vector<float> r_gpu(n);
+    std::fill(r_cpu.begin(), r_cpu.end(), 0);
+    std::fill(r_gpu.begin(), r_gpu.end(), 0);
+
+    //
+    // Data preparation on CPU device and GPU device
+    //
+    float* dev_cpu = sycl::malloc_device<float>(n * sizeof(float), cpu_queue);
+    float* dev_gpu = sycl::malloc_device<float>(n * sizeof(float), gpu_queue);
+    if (!dev_cpu || !dev_gpu) {
+        throw std::runtime_error("Failed to allocate USM memory.");
+    }
+
+    //
+    // Perform generation on CPU device and GPU device
+    //
+    sycl::event event_out_cpu;
+    sycl::event event_out_gpu;
+    event_out_cpu = oneapi::mkl::rng::generate(distribution, cpu_engine, n, dev_cpu);
+    event_out_gpu = oneapi::mkl::rng::generate(distribution, gpu_engine, n, dev_gpu);
+    event_out_cpu.wait_and_throw();
+    event_out_gpu.wait_and_throw();
+
+    //
+    // Post Processing
+    //
+
+    // copy data from CPU device and GPU device back to host
+    cpu_queue.memcpy(r_cpu.data(), dev_cpu, n * sizeof(float)).wait_and_throw();
+    gpu_queue.memcpy(r_gpu.data(), dev_gpu, n * sizeof(float)).wait_and_throw();
+
+    std::cout << "\t\tgeneration parameters:" << std::endl;
+    std::cout << "\t\t\tseed = " << seed << ", a = " << a << ", b = " << b << std::endl;
+
+    std::cout << "\t\tOutput of generator on CPU device:" << std::endl;
+    std::cout << "\t\t\tfirst " << n_print << " numbers of " << n << ": " << std::endl;
+    for (int i = 0; i < n_print; i++) {
+        std::cout << r_cpu.at(i) << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "\t\tOutput of generator on GPU device:" << std::endl;
+    std::cout << "\t\t\tfirst " << n_print << " numbers of " << n << ": " << std::endl;
+    for (int i = 0; i < n_print; i++) {
+        std::cout << r_gpu.at(i) << " ";
+    }
+    std::cout << std::endl;
+
+    sycl::free(dev_gpu, gpu_queue);
+    sycl::free(dev_cpu, cpu_queue);
+
+}
+
+//
+// Description of example setup, APIs used and supported floating point type precisions
+//
+void print_example_banner() {
+    std::cout << "" << std::endl;
+    std::cout << "########################################################################"
+              << std::endl;
+    std::cout
+        << "# Generate uniformly distributed random numbers with philox4x32x10\n# generator example: "
+        << std::endl;
+    std::cout << "# " << std::endl;
+    std::cout << "# Using APIs:" << std::endl;
+    std::cout << "#   default_engine uniform" << std::endl;
+    std::cout << "# " << std::endl;
+    std::cout << "# Using single precision (float) data type" << std::endl;
+    std::cout << "# " << std::endl;
+    std::cout << "# Running on both Intel CPU and Nvidia GPU devices" << std::endl;
+    std::cout << "# " << std::endl;
+    std::cout << "########################################################################"
+              << std::endl;
+    std::cout << std::endl;
+}
+
+//
+// Main entry point for example.
+//
+
+int main(int argc, char** argv) {
+    print_example_banner();
+    try {
+        sycl::device cpu_dev((sycl::cpu_selector()));
+        sycl::device gpu_dev((sycl::gpu_selector()));
+
+        unsigned int vendor_id = gpu_dev.get_info<sycl::info::device::vendor_id>();
+        if (vendor_id != NVIDIA_ID) {
+            std::cerr << "FAILED: NVIDIA GPU device not found" << std::endl;
+            return 1;
+        }
+        std::cout << "Running RNG uniform usm example" << std::endl;
+        std::cout << "Running with single precision real data type:" << std::endl;
+        std::cout << "\tCPU device: " << cpu_dev.get_info<sycl::info::device::name>() << std::endl;
+        std::cout << "\tGPU device: " << gpu_dev.get_info<sycl::info::device::name>() << std::endl;
+
+        run_uniform_example(cpu_dev, gpu_dev);
+        std::cout
+            << "Random number generator example with uniform distribution ran OK on MKLCPU and CURAND"
+            << std::endl;
+    }
+    catch (sycl::exception const& e) {
+        std::cerr << "Caught synchronous SYCL exception during generation:" << std::endl;
+        std::cerr << "\t" << e.what() << std::endl;
+        std::cerr << "\tSYCL error code: " << e.code().value() << std::endl;
+        return 1;
+    }
+    catch (std::exception const& e) {
+        std::cerr << "Caught std::exception during generation:" << std::endl;
+        std::cerr << "\t" << e.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
