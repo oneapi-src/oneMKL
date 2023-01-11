26af7938b08ecbfde74268a565d18f6a4b514bb9
Hugh Bird <hugh.bird@codeplay.com>
2023-01-11 16:45:32 +0000

    Update Copyright dates

--- include/oneapi/mkl/dft/detail/dft_ct.hxx
+++ include/oneapi/mkl/dft/detail/dft_ct.hxx
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2022 Codeplay Software
+* Copyright 2023 Codeplay Software
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
