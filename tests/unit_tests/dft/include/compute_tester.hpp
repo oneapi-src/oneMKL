7b4ca33ed331f68ae2ab4baf1a24b9e023853a9e
Hugh Bird <hugh.bird@codeplay.com>
2023-01-16 18:06:03 +0000

    Move DFT_Test function definitions into class definition

--- tests/unit_tests/dft/include/compute_tester.hpp
+++ tests/unit_tests/dft/include/compute_tester.hpp
@@ -60,10 +60,52 @@ struct DFT_Test {
     std::vector<PrecisionType> input_im;
     std::vector<OutputType> out_host_ref;
 
-    DFT_Test(sycl::device *dev, std::int64_t size);
+    DFT_Test(sycl::device *dev, std::int64_t size)
+            : dev{ dev },
+              size{ static_cast<std::int64_t>(size) },
+              conjugate_even_size{ 2 * (size / 2 + 1) },
+              sycl_queue{ *dev, exception_handler },
+              cxt{ sycl_queue.get_context() } {
+        input = std::vector<InputType>(size);
+        input_re = std::vector<PrecisionType>(size);
+        input_im = std::vector<PrecisionType>(size);
 
-    bool skip_test(TestType type);
-    bool init(TestType type);
+        out_host_ref = std::vector<OutputType>(size);
+        rand_vector(input, size, 1);
+
+        if constexpr (domain == oneapi::mkl::dft::domain::REAL) {
+            for (int i = 0; i < input.size(); ++i) {
+                input_re[i] = { input[i] };
+                input_im[i] = 0;
+            }
+        }
+        else {
+            for (int i = 0; i < input.size(); ++i) {
+                input_re[i] = { input[i].real() };
+                input_im[i] = { input[i].imag() };
+            }
+        }
+    }
+
+    bool skip_test(TestType type) {
+        if constexpr (precision == oneapi::mkl::dft::precision::DOUBLE) {
+            if (!sycl_queue.get_device().has(sycl::aspect::fp64)) {
+                std::cout << "Device does not support double precision." << std::endl;
+                return true;
+            }
+        }
+        if (type == TestType::usm &&
+            !sycl_queue.get_device().has(sycl::aspect::usm_shared_allocations)) {
+            std::cout << "Device does not support usm shared allocations." << std::endl;
+            return true;
+        }
+        return false;
+    }
+
+    bool init(TestType type) {
+        reference_forward_dft<InputType, OutputType>(input, out_host_ref);
+        return !skip_test(type);
+    }
 
     int test_in_place_buffer();
     int test_in_place_real_real_buffer();
@@ -74,57 +116,5 @@ struct DFT_Test {
     int test_out_of_place_USM();
     int test_out_of_place_real_real_USM();
 };
-
-template <oneapi::mkl::dft::precision precision, oneapi::mkl::dft::domain domain>
-DFT_Test<precision, domain>::DFT_Test(sycl::device *dev, std::int64_t size)
-        : dev{ dev },
-          size{ static_cast<std::int64_t>(size) },
-          conjugate_even_size{ 2 * (size / 2 + 1) },
-          sycl_queue{ *dev, exception_handler },
-          cxt{ sycl_queue.get_context() } {
-    input = std::vector<InputType>(size);
-    input_re = std::vector<PrecisionType>(size);
-    input_im = std::vector<PrecisionType>(size);
-
-    out_host_ref = std::vector<OutputType>(size);
-    rand_vector(input, size, 1);
-
-    if constexpr (domain == oneapi::mkl::dft::domain::REAL) {
-        for (int i = 0; i < input.size(); ++i) {
-            input_re[i] = { input[i] };
-            input_im[i] = 0;
-        }
-    }
-    else {
-        for (int i = 0; i < input.size(); ++i) {
-            input_re[i] = { input[i].real() };
-            input_im[i] = { input[i].imag() };
-        }
-    }
-}
-
-template <oneapi::mkl::dft::precision precision, oneapi::mkl::dft::domain domain>
-bool DFT_Test<precision, domain>::skip_test(TestType type) {
-    if constexpr (precision == oneapi::mkl::dft::precision::DOUBLE) {
-        if (!sycl_queue.get_device().has(sycl::aspect::fp64)) {
-            std::cout << "Device does not support double precision." << std::endl;
-            return true;
-        }
-    }
-
-    if (type == TestType::usm &&
-        !sycl_queue.get_device().has(sycl::aspect::usm_shared_allocations)) {
-        std::cout << "Device does not support usm shared allocations." << std::endl;
-        return true;
-    }
-
-    return false;
-}
-
-template <oneapi::mkl::dft::precision precision, oneapi::mkl::dft::domain domain>
-bool DFT_Test<precision, domain>::init(TestType type) {
-    reference_forward_dft<InputType, OutputType>(input, out_host_ref);
-    return !skip_test(type);
-}
 
 #endif //ONEMKL_COMPUTE_TESTER_HPP
