deed2d7420e0b8ee1c6dab4269b749b1ce10e513
Meterelliyoz, Mesut <mesut.meterelliyoz@intel.com>
2023-04-05 11:04:38 -0700

    [BLAS] update copyright

--- src/blas/backends/cublas/cublas_task.hpp
+++ src/blas/backends/cublas/cublas_task.hpp
@@ -1,5 +1,7 @@
 /***************************************************************************
 *  Copyright (C) Codeplay Software Limited
+*  Copyright (C) 2022 Heidelberg University, Engineering Mathematics and Computing Lab (EMCL) and Computing Centre (URZ)
+*
 *  Licensed under the Apache License, Version 2.0 (the "License");
 *  you may not use this file except in compliance with the License.
 *  You may obtain a copy of the License at
