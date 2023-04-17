1523afc8598b08065a0177932ec45762bf893a22
Andrei Fedorov <andrey.fedorov@intel.com>
2023-04-17 16:52:07 +0200

    Remove cuda backend include from mrg32k3a.cpp

--- src/rng/backends/curand/mrg32k3a.cpp
+++ src/rng/backends/curand/mrg32k3a.cpp
@@ -62,9 +62,7 @@
 #include <CL/sycl.hpp>
 #endif
 #ifndef __HIPSYCL__
-#if __has_include(<sycl/backend/cuda.hpp>)
-#include <sycl/backend/cuda.hpp>
-#else
+#if !__has_include(<sycl/backend/cuda.hpp>)
 #include <CL/sycl/backend/cuda.hpp>
 #endif
 #endif
