81e540449747db16ca68942885b631163622fc70
Hugh Bird <hugh.bird@codeplay.com>
2023-07-10 12:26:02 +0100

    [DFT] Add descriptor move constructor and assignment
    * Adds dft::descriptor move constructor
    * Adds dft::descriptor move assignment
    * Adds basic tests for both
    * Notes that copy is not implemented

--- include/oneapi/mkl/dft/detail/descriptor_impl.hpp
+++ include/oneapi/mkl/dft/detail/descriptor_impl.hpp
@@ -52,6 +52,14 @@ public:
     // Syntax for d-dimensional DFT
     descriptor(std::vector<std::int64_t> dimensions);
 
+    // Copy operations are included in the oneAPI oneMKL specification, but not yet
+    // implemented here. If you need copies, please open an issue at
+    // https://github.com/oneapi-src/oneMKL/issues
+
+    descriptor(descriptor&&);
+
+    descriptor& operator=(descriptor&&);
+
     ~descriptor();
 
     void set_value(config_param param, ...);
