a78d64d09b45c5e1cef917f9511e59feb212e6ec
HJA Bird <9040797+hjabird@users.noreply.github.com>
2023-07-27 10:26:30 +0100

    [DFT] Fix MKLCPU const_cast for release DPC++ (#356)
    * A previous commit introduces const_cast to fix changes to nightly LLVM
      * This break builds with the DPC++ release compiler
    * This PR fixes the const casts.

--- src/dft/backends/mklcpu/forward.cpp
+++ src/dft/backends/mklcpu/forward.cpp
@@ -157,7 +157,7 @@ ONEMKL_EXPORT void compute_forward(descriptor_type &desc, sycl::buffer<input_typ
         auto out_acc = out.template get_access<sycl::access::mode::write>(cgh);
 
         detail::host_task<class host_kernel_outofplace>(cgh, [=]() {
-            auto in_ptr = const_cast<input_type *>(in_acc.get_pointer());
+            auto in_ptr = const_cast<input_type *>(&in_acc.get_pointer()[0]);
             DFT_ERROR status =
                 DftiComputeForward(desc_acc[detail::DIR::fwd], in_ptr, out_acc.get_pointer());
             if (status != DFTI_NO_ERROR) {
@@ -193,8 +193,8 @@ ONEMKL_EXPORT void compute_forward(descriptor_type &desc, sycl::buffer<input_typ
         auto outim_acc = out_im.template get_access<sycl::access::mode::write>(cgh);
 
         detail::host_task<class host_kernel_split_outofplace>(cgh, [=]() {
-            auto inre_ptr = const_cast<input_type *>(inre_acc.get_pointer());
-            auto inim_ptr = const_cast<input_type *>(inim_acc.get_pointer());
+            auto inre_ptr = const_cast<input_type *>(&inre_acc.get_pointer()[0]);
+            auto inim_ptr = const_cast<input_type *>(&inim_acc.get_pointer()[0]);
             DFT_ERROR status = DftiComputeForward(desc_acc[detail::DIR::fwd], inre_ptr, inim_ptr,
                                                   outre_acc.get_pointer(), outim_acc.get_pointer());
             if (status != DFTI_NO_ERROR) {
