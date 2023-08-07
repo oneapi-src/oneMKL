e0b3ca90be433ce78810f04051ed72cd363f3b33
Tadej Ciglarič <tadej.ciglaric@codeplay.com>
2023-08-07 12:33:02 +0200

    improve offset tests anf fix real offsets

--- tests/unit_tests/dft/source/compute_tests.cpp
+++ tests/unit_tests/dft/source/compute_tests.cpp
@@ -113,19 +113,10 @@ std::vector<DFTParams> test_params{
     { shape{ 4, 4 }, shape{ 3, 8, 2}, shape{ 2, 8, 2}, i64{ 2 } },
     { shape{ 4, 4 }, shape{ 0, 4, 1}, shape{ 0, 1, 4}, i64{ 2 } },
 
-    { shape{ 4, 4, 4 }, shape{ 0, 1, 4, 16}, shape{ 0, 1, 4, 16}, i64{ 2 } },
-    { shape{ 4, 4, 4 }, shape{ 0, 17, 4, 1}, shape{ 0, 23, 5, 1}, i64{ 2 } },
-    { shape{ 4, 4, 4 }, shape{ 0, 32, 8, 2}, shape{ 0, 32, 8, 2}, i64{ 2 } },
+    { shape{ 4, 4, 4 }, shape{ 2, 1, 4, 16}, shape{ 4, 1, 4, 16}, i64{ 2 } },
+    { shape{ 4, 4, 4 }, shape{ 4, 17, 4, 1}, shape{ 2, 23, 5, 1}, i64{ 2 } },
+    { shape{ 4, 4, 4 }, shape{ 4, 32, 8, 2}, shape{ 4, 32, 8, 2}, i64{ 2 } },
     { shape{ 4, 4, 4 }, shape{ 0, 4, 1, 16}, shape{ 0, 4, 16, 1}, i64{ 2 } },
-    /*{ shape{ 4, 4 }, shape{ 1, 1, 4}, shape{ 1, 1, 4}, i64{ 2 } },
-    { shape{ 4, 4 }, shape{ 0, 7, 1}, shape{ 0, 5, 1}, i64{ 2 } },
-    { shape{ 4, 4 }, shape{ 0, 8, 2}, shape{ 0, 8, 2}, i64{ 2 } },
-    { shape{ 4, 4 }, shape{ 1, 4, 1}, shape{ 1, 1, 4}, i64{ 2 } },
-
-    { shape{ 4, 4, 4 }, shape{ 1, 1, 4, 16}, shape{ 1, 1, 4, 16}, i64{ 2 } },
-    { shape{ 4, 4, 4 }, shape{ 0, 17, 4, 1}, shape{ 0, 23, 5, 1}, i64{ 2 } },
-    { shape{ 4, 4, 4 }, shape{ 0, 32, 8, 2}, shape{ 0, 32, 8, 2}, i64{ 2 } },
-    { shape{ 4, 4, 4 }, shape{ 1, 4, 1, 16}, shape{ 1, 4, 16, 1}, i64{ 2 } },*/
 };
 
 INSTANTIATE_TEST_SUITE_P(ComputeTestSuite, ComputeTests_in_place,
