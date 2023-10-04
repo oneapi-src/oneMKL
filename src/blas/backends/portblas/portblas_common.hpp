6793a49de98874dec7d9f39e3b539ac90235d608
aacostadiaz <127198532+aacostadiaz@users.noreply.github.com>
2023-10-04 09:52:06 +0100

    [BLAS::portBLAS backend] Add USM support for level 1 operators (#370)
    Co-authored-by: nscipione <nicolo.scipione@codeplay.com>

--- src/blas/backends/portblas/portblas_common.hpp
+++ src/blas/backends/portblas/portblas_common.hpp
@@ -74,6 +74,16 @@ struct portblas_type<sycl::buffer<ElemT, 1>> {
     using type = buffer_iterator_t<ElemT>;
 };
 
+template <typename ElemT>
+struct portblas_type<ElemT*> {
+    using type = ElemT*;
+};
+
+template <>
+struct portblas_type<std::vector<sycl::event>> {
+    using type = std::vector<sycl::event>;
+};
+
 /** Convert a OneMKL argument to the type required for portBLAS.
  *  
  *  @tparam InputT The OneMKL type.
@@ -175,7 +185,23 @@ struct throw_if_unsupported_by_device {
         std::apply(fn, args);                                                                   \
     }                                                                                           \
     else {                                                                                      \
-        throw unimplemented("blas", "portBLAS function", " for row-major");                     \
+        throw unimplemented("blas", "portBLAS function");                                       \
+    }
+
+#define CALL_PORTBLAS_USM_FN(portblasFunc, ...)                                   \
+    if constexpr (is_column_major()) {                                            \
+        detail::throw_if_unsupported_by_device<double, sycl::aspect::fp64>{}(     \
+            " portBLAS function requiring fp64 support", __VA_ARGS__);            \
+        detail::throw_if_unsupported_by_device<sycl::half, sycl::aspect::fp16>{}( \
+            " portBLAS function requiring fp16 support", __VA_ARGS__);            \
+        auto args = detail::convert_to_portblas_type(__VA_ARGS__);                \
+        auto fn = [](auto&&... targs) {                                           \
+            return portblasFunc(std::forward<decltype(targs)>(targs)...).back();  \
+        };                                                                        \
+        return std::apply(fn, args);                                              \
+    }                                                                             \
+    else {                                                                        \
+        throw unimplemented("blas", "portBLAS function");                         \
     }
 
 } // namespace portblas
