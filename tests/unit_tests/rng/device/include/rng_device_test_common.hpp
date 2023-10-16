6858a48450399b14720afd15173cbfd7dbd76cbf
Grabovskaya, Ekaterina <ekaterina.grabovskaya@intel.com>
2023-10-16 08:31:50 -0700

    ENABLE_CURAND_BACKEND code was removed

--- tests/unit_tests/rng/device/include/rng_device_test_common.hpp
+++ tests/unit_tests/rng/device/include/rng_device_test_common.hpp
@@ -92,22 +92,7 @@ public:
             }
         };
 
-// #ifdef ENABLE_CURAND_BACKEND // w/a for cuda backend hangs when there are several queues with different contexts
-//         static sycl::device* previous_device = nullptr;
-//         static sycl::context* context = nullptr;
-
-//         if ((previous_device != dev)) {
-//             previous_device = dev;
-//             if (context != nullptr) {
-//                 delete context;
-//             }
-//             context = new sycl::context(*dev);
-//         }
-
-//         sycl::queue queue(*context, *dev, exception_handler);
-// #else
         sycl::queue queue(*dev, exception_handler);
-// #endif
 
         test_(queue, args...);
 
