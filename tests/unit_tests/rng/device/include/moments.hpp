5b9d6a367fc6436ae0e00be4484ef68f4d6bb119
Grabovskaya, Ekaterina <ekaterina.grabovskaya@intel.com>
2023-10-17 08:54:08 -0700

    Revert "SFINAE for get_multi_ptr/get_pointer"
    This reverts commit 69f4806bae0574dc399ba722676c49ba0067c056.

--- tests/unit_tests/rng/device/include/moments.hpp
+++ tests/unit_tests/rng/device/include/moments.hpp
@@ -67,7 +67,7 @@ public:
                         acc[id] = res;
                     }
                     else {
-                        res.store(id, get_multi_ptr(acc));
+                        res.store(id, acc);
                     }
                 });
             });
--- tests/unit_tests/rng/device/include/skip_ahead_test.hpp
+++ tests/unit_tests/rng/device/include/skip_ahead_test.hpp
@@ -68,7 +68,7 @@ public:
                         acc[id] = res;
                     }
                     else {
-                        res.store(id, get_multi_ptr(acc));
+                        res.store(id, acc);
                     }
                 });
             });
@@ -131,7 +131,7 @@ public:
                         acc[id] = res;
                     }
                     else {
-                        res.store(id, get_multi_ptr(acc));
+                        res.store(id, acc);
                     }
                 });
             });
