0e5043bd4079d181abadc137aa02dc345072456e
Jefferson Le Quellec <jefferson.lequellec@codeplay.com>
2023-10-18 19:19:55 +0200

    [LAPACK][rocSOLVER] Update rocSOLVER backend (#393)

--- src/lapack/backends/rocsolver/rocsolver_scope_handle.hpp
+++ src/lapack/backends/rocsolver/rocsolver_scope_handle.hpp
@@ -57,7 +57,7 @@ public:
     // will be fixed when SYCL-2020 has been implemented for Pi backend.
     template <typename T, typename U>
     inline T get_mem(U acc) {
-        hipDeviceptr_t hipPtr = ih.get_native_mem<sycl::backend::hip>(acc);
+        hipDeviceptr_t hipPtr = ih.get_native_mem<sycl::backend::ext_oneapi_hip>(acc);
         return reinterpret_cast<T>(hipPtr);
     }
 };
