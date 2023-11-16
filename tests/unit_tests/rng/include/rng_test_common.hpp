244204055d0886925636d0bfbdb9f65a53fdcbd7
Elizarova, Alina <alina.elizarova@intel.com>
2023-11-16 09:12:31 -0800

    clang format for test

--- tests/unit_tests/rng/include/rng_test_common.hpp
+++ tests/unit_tests/rng/include/rng_test_common.hpp
@@ -123,9 +123,9 @@ protected:
 };
 
 #ifdef CALL_RT_API
-#define QUEUE_WAIT(q)   q.wait()
+#define QUEUE_WAIT(q) q.wait()
 #else
-#define QUEUE_WAIT(q)   q.get_queue().wait()
+#define QUEUE_WAIT(q) q.get_queue().wait()
 #endif
 
 #endif // _RNG_TEST_COMMON_HPP__
