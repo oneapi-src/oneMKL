475956b1e141b43b1803a815de1f9aaabc4c1a12
Hugh Bird <hugh.bird@codeplay.com>
2023-11-22 10:29:19 +0000

    Improve comment on default behaviour

--- include/oneapi/mkl/dft/detail/commit_impl.hpp
+++ include/oneapi/mkl/dft/detail/commit_impl.hpp
@@ -84,6 +84,9 @@ public:
     // set_workspace should be overrided for any backend that enables external workspaces.
     // If these are overrided, get_workspace_external_bytes_impl must also be overrided.
     // For backends that do not support external workspaces, these functions do not need to be overrided.
+    // When not overrided, external workspace support is faked: an external workspace can be set, 
+    // and errors will be generated according to the specificiation, 
+    // but the required workspace size will always be zero, and any given workspace will not actually be used.
     virtual void set_workspace(scalar_type *usm_workspace) {
         external_workspace_helper_.set_workspace_throw(*this, usm_workspace);
     };
