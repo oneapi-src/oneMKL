3e924d505e3c6153fc94a7cdc3ffa5e3a4851c07
Andrei Fedorov <andrey.fedorov@intel.com>
2023-12-14 14:26:30 +0100

    [RNG] Removed deprecated API from Device API tests and examples (#427)

--- examples/rng/device/include/rng_example_helper.hpp
+++ examples/rng/device/include/rng_example_helper.hpp
@@ -34,7 +34,7 @@ auto get_multi_ptr(T acc) {
 
 template <typename T, typename std::enable_if<!has_member_code_meta<T>::value>::type* = nullptr>
 auto get_multi_ptr(T acc) {
-    return acc.get_pointer();
+    return acc.template get_multi_ptr<sycl::access::decorated::yes>();
 };
 
 #endif // _RNG_EXAMPLE_HELPER_HPP__
