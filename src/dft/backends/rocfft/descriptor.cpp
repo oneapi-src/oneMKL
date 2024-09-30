00a9af74d4089cdfa988d0ad4268ec4980c9e0e7
Nicolò Scipione <nicolo.scipione@codeplay.com>
2024-09-30 15:09:59 +0200

    [DFT][rocFFt] Address rocFFT failing tests (#563)
    Co-authored-by: Hugh Bird <hugh.bird@codeplay.com>

--- src/dft/backends/rocfft/descriptor.cpp
+++ src/dft/backends/rocfft/descriptor.cpp
@@ -22,9 +22,7 @@
 
 #include "oneapi/mkl/dft/detail/rocfft/onemkl_dft_rocfft.hpp"
 
-namespace oneapi {
-namespace mkl {
-namespace dft {
+namespace oneapi::mkl::dft::detail {
 
 template <precision prec, domain dom>
 void descriptor<prec, dom>::commit(backend_selector<backend::rocfft> selector) {
@@ -46,6 +44,4 @@ template void descriptor<precision::DOUBLE, domain::COMPLEX>::commit(
 template void descriptor<precision::DOUBLE, domain::REAL>::commit(
     backend_selector<backend::rocfft>);
 
-} //namespace dft
-} //namespace mkl
-} //namespace oneapi
+} //namespace oneapi::mkl::dft::detail
