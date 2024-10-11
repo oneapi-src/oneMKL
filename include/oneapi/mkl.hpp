228d214a5bf3c2c7ced8593d27abbb8797fb07a7
romain.biessy <romain.biessy@codeplay.com>
2024-10-11 12:02:18 +0200

    Add deprecated headers and mkl namespace

---
+++ include/oneapi/mkl.hpp
@@ -0,0 +1,36 @@
+/*******************************************************************************
+* Copyright 2020-2021 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions
+* and limitations under the License.
+*
+*
+* SPDX-License-Identifier: Apache-2.0
+*******************************************************************************/
+
+#ifndef ONEMATH_MKL_HPP
+#define ONEMATH_MKL_HPP
+
+// Deprecated header is planned to be removed late 2025.
+#pragma message("Header `oneapi/mkl.hpp` is deprecated, please use `oneapi/math.hpp` instead")
+
+#include "oneapi/math/types.hpp"
+
+#include "oneapi/math/blas.hpp"
+#include "oneapi/math/dft.hpp"
+#include "oneapi/math/lapack.hpp"
+#include "oneapi/math/rng.hpp"
+#include "oneapi/math/sparse_blas.hpp"
+
+#include "namespace_alias.hpp"
+
+#endif // ONEMATH_MKL_HPP
---
+++ include/oneapi/mkl/lapack.hpp
@@ -0,0 +1,30 @@
+/*******************************************************************************
+* Copyright 2024 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions
+* and limitations under the License.
+*
+*
+* SPDX-License-Identifier: Apache-2.0
+*******************************************************************************/
+
+#ifndef ONEMATH_MKL_LAPACK_HPP
+#define ONEMATH_MKL_LAPACK_HPP
+
+// Deprecated header is planned to be removed late 2025.
+#pragma message("Header `oneapi/mkl/lapack.hpp` is deprecated, please use `oneapi/math/lapack.hpp` instead")
+
+#include "oneapi/math/lapack.hpp"
+
+#include "namespace_alias.hpp"
+
+#endif // ONEMATH_MKL_LAPACK_HPP
